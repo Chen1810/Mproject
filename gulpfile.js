6e151cafec2b61b59d1881eefa1db7b936efe083
unknown <>
2018-12-07 16:49:07 +0800

    增加了gulp-webpack

--- gulpfile.js
+++ gulpfile.js
@@ -1,63 +1,97 @@
+
 let gulp = require("gulp");
-let uglify = require("gulp-uglify");
-let babel = require("gulp-babel");
+let uglify = require("gulp-uglify"); //压缩模块
+let babel = require("gulp-babel"); //ES6的编译模块
 let cleancss = require("gulp-clean-css");
 let webserver = require("gulp-webserver");
-let sass = require("gulp-sass");//编译scss到css
+let sass = require("gulp-sass"); //编译SCSS到CSS
 
-gulp.task("copy",()=>{
-	gulp.src("./src/**/*.*").pipe(gulp.dest("./dist"))
-})
-
-gulp.task("buildJS",()=>{
+gulp.task("buildJS", ()=>{
 	//只复制
 	gulp.src("./src/scripts/libs/*.js")
-	.pipe(gulp.dest("./dist/scripts/libs"))
+		.pipe( gulp.dest("./dist/scripts/libs") )
+	
 	//编译压缩复制
 	gulp.src("./src/scripts/*.js")
-	.pipe(babel({
-		presets:['env']
-	}))
-	.pipe(uglify())
-	.pipe(gulp.dest("./dist/scripts"));
+		.pipe(babel({
+            presets: ['env']
+        }))
+		.pipe( uglify() )
+		.pipe( gulp.dest("./dist/scripts") );
 })
 
-gulp.task("buildCSS",()=>{
+gulp.task("buildCSS", ()=>{
+	
 	gulp.src("./src/**/*.scss")
-	//.pipe(cleancss())
-	.pipe(sass())
-	.pipe(gulp.dest("./dist"))
+		// .pipe(cleancss())
+		.pipe(sass())
+		.pipe( gulp.dest("./dist") )
+	
 })
 
-gulp.task("buildHTML",()=>{
-	gulp.src("./src/**/*.html").pipe(gulp.dest("./dist"))
-})
-//处理静态资源
-gulp.task("buildStaticResource",()=>{
-	gulp.src("./src/static/**/*.*").pipe(gulp.dest("./dist"));
+gulp.task("buildHTML", ()=>{
+	gulp.src("./src/**/*.html").pipe( gulp.dest("./dist") );
 })
 
-//gulp.watch("./src/**/*.scss",["buildCSS"]);
-//添加监听事件,修改src中的html css js时 dist中对应项同时发生变化
-gulp.task("watching",()=>{
-	gulp.watch("./src/**/*.scss",["buildCSS"]);
-	gulp.watch("./src/**/*.js",["buildJS"]);
-	gulp.watch("./src/**/*.html",["buildHTML"]);
+gulp.task("buildStaticResource", ()=>{
+	gulp.src("./src/static/**/*.*").pipe( gulp.dest("./dist") );
 })
 
-gulp.task("webserver",["watching"],()=>{
-	gulp.src("dist").pipe(webserver({
-		livereload:true,
-		https:true,
-		port:8090,
-		proxies:[
-			{
-				source:"/abcdefg",
-				target:"https://m.lagou.com/listmore.json",
-			}
-		]
-	}))
+gulp.task("watching", ()=>{
+	gulp.watch("./src/**/*.scss", ["buildCSS"]);
+	gulp.watch("./src/**/*.js", ["buildJS"]);
+	gulp.watch("./src/**/*.html", ["buildHTML"]);
+});
+
+const webpack = require("webpack-stream");
+
+gulp.task("webpackJS",()=>{
+	webpack({
+		mode: "development",  //模式
+		entry: ["./src/webpack/app.js"],  //配置入口文件，打包时webpack从哪个文件开始读取
+		output: {
+		  	filename: 'app.js', //打包生产后的文件名称
+		},
+		module:{
+				rules:  [
+				{
+					test: /\.js$/,   //匹配.js文件
+					exclude: /(node_modules|bower_components)/,   //将node_modules里的JS排除
+				  	use: {  //配置babel-loader用来编译ES6、7语法
+						loader: 'babel-loader',  
+						options: {
+							presets: ['@babel/preset-env']
+						}
+				  	}
+				},
+				{
+					test: /\.scss$/,   //匹配.js文件
+				  	use: ["style-loader","css-loader","sass-loader"]
+				},
+			]
+		}
+	}).pipe(gulp.dest("./src/webpack/dist"))
 })
 
-gulp.task("build",["buildJS","buildHTML","buildCSS","buildStaticResource"])
+//yintao01分支下的修改
+gulp.task('webserver', ["watching"], function() {
+	gulp.src('dist')
+		.pipe(webserver({
+			livereload: true, //是否支持热部署
+			port:8090,
+			https: true,      //
+			proxies : [
+				{	
+					source: '/abcdefg', 
+					target: 'https://m.lagou.com/listmore.json',
+				},
+				{
+					source: '/userinfo',
+					target: 'https://nbrecsys.4paradigm.com/api/v0/recom/recall?requestID=pmKC7kYD&userID=u3FFkObPEe&sceneID=34'
+					
+				}
+			]
+		}));
+});
 
+gulp.task("build", ["buildJS","buildHTML", "buildCSS", "buildStaticResource"])
---
+++ src/webpack/app.js
@@ -0,0 +1,18 @@
+import scroll from "./scroll.js";
+import "./scss/add.scss";
+
+
+scroll.run();
+
+//node.js语法
+const title = require("./title.js")
+title();
+
+class test{
+	study(){
+		console.log("shanghai")
+	}
+}
+new test().study();
+
+import "./scss/add.scss";
